067ff2c6594bac483c7a3b297267b777666af412
Jarjuna95 <jarjuna05@gmail.com>
2025-04-28 22:16:07 +0200

    Update projet_fonction.c

--- Projet/projet_fonction.c
+++ Projet/projet_fonction.c
@@ -1,4 +1,30 @@
 #include "projet.h"
+void RechercheAnimal(int critere){
+     if(critere==1){
+        char nomRecherche[50];
+        printf("Entrez le nom de l'animal à chercher : \n");
+        scanf("%s", nomRecherche);
+        if(nomRecherche[0]>='a' || nomRecherche[0]<='z'){
+          nomRecherche[0]-=32;
+        }
+        for (int j = 0; j < i; j++) {
+        if (strcmp(refuge[j].nom, nomRecherche) == 0) {
+            printf("Animal trouvé : %d, %s, %d, %d, %.2f, %s\n", 
+                   refuge[j].numero_id, 
+                   refuge[j].nom, 
+                   refuge[j].espece, 
+                   refuge[j].anniv, 
+                   refuge[j].poids, 
+                   refuge[j].comm);
+            trouve = 1;
+            break;
+        }
+    }
 
+    if (!trouve) {
+        printf("Animal non trouvé.\n");
+    }
+    ;
+        } */
 
       
