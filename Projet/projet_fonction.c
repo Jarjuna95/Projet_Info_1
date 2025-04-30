801d98c4c8ce2e800825f50fd908484537d73e0c
Jarjuna95 <jarjuna05@gmail.com>
2025-04-30 14:05:23 +0200

    Update projet_fonction.c

--- Projet/projet_fonction.c
+++ Projet/projet_fonction.c
@@ -4,8 +4,54 @@ void AfficheAnimal(int indice){
         printf("%d, %s, %d, %d, %f, %s \n", refuge[indice].numero_id, refuge[indice].nom, refuge[indice].espece, refuge[indice].anniv, refuge[indice].poids, refuge[indice].comm);
 }
  
-void RechercheAnimal(int critere){
-     if(critere==1){
+void RechercheAnimal(){
+     char nomRecherche[50];
+     int espece;
+     printf("Entrez le nom de l'animal à chercher : \n");
+     scanf("%s", nomRecherche);
+        if(nomRecherche[0]>='a' || nomRecherche[0]<='z'){
+          nomRecherche[0]-=32;
+        }
+     printf("Quel espèce cherchez-vous ? (0=chien; 1=chat; 2=hamster; 3=autruche\n)";
+     scanf("%d", &espece);
+        while(espece<0 && espece>3){
+             printf("Erreur veuillez ressaisir :\n);
+             scanf("%d", &espece);
+        }
+     printf("Le type d'âge ? (1 pour sénior, 2 pour jeune) \n");
+            
+        
+
+      
+
+         
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*void RechercheAnimal(int critere){    
+
+        if(critere==1){
         char nomRecherche[50];
         printf("Entrez le nom de l'animal à chercher : \n");
         scanf("%s", nomRecherche);
@@ -22,7 +68,7 @@ void RechercheAnimal(int critere){
         printf("Animal non trouvé.\n");
      }
      else if(critere==2){ 
-          int espece;
+          int espece;*/
           
         
 
