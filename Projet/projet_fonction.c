02dcfa9fcdb516699b3ee118d38d8a664f693f17
Jarjuna95 <jarjuna05@gmail.com>
2025-04-30 15:02:27 +0200

    Update projet_fonction.c

--- Projet/projet_fonction.c
+++ Projet/projet_fonction.c
@@ -4,6 +4,23 @@ void AfficheAnimal(int indice){
         printf("%d, %s, %d, %d, %f, %s \n", refuge[indice].numero_id, refuge[indice].nom, refuge[indice].espece, refuge[indice].anniv, refuge[indice].poids, refuge[indice].comm);
 }
  
+int Age(int h){
+        if(2025-refuge[h].anniv>2){
+                  return 2;
+          }
+          else if(2025-refuge[h].anniv<10){
+                  return 1;
+          }
+          else{
+                  return 3;
+          }
+   }
+}
+
+
+
+
+
 void RechercheAnimal(){
      char nomRecherche[50];
      int espece, age;
@@ -24,32 +41,16 @@ void RechercheAnimal(){
              printf("Erreur veuillez ressaisir :\n);
              scanf("%d", &age);        
         }
-     for (int j = 0; j < 2; j++) {                                       // changer le 2 (nombre d'animal au total
-            if (strcmp(refuge[j].nom, nomRecherche) == 0 && refuge[j].espece==espece && (2025-refuge[j].anniv) {                     //comparer le nom avec les animaux du refuge
+     for (int j = 0; j < 2; j++) {                                       // changer le 2 (nombre d'animal au total     
+         if (strcmp(refuge[j].nom, nomRecherche) == 0 && refuge[j].espece==espece && Age(j)==age){                     //comparer le nom avec les animaux du refuge
                printf("Animal trouvé voici les informations : \n");
                AfficheAnimal(j);
-               break;
-            }
-        }
+         }
+      }
+      printf("Non trouvé \n);
+}
 
-/*void RechercheAnimal(int critere){    
-        printf("Entrez le nom de l'animal à chercher : \n");
-        scanf("%s", nomRecherche);
-        if(nomRecherche[0]>='a' || nomRecherche[0]<='z'){
-          nomRecherche[0]-=32;
-        }
-        for (int j = 0; j < 2; j++) {                                       // changer le 2 (nombre d'animal au total
-            if (strcmp(refuge[j].nom, nomRecherche) == 0) {                     //comparer le nom avec les animaux du refuge
-               printf("Animal trouvé voici les informations : \n");
-               AfficheAnimal(j);
-               break;
-            }
-        }
-        printf("Animal non trouvé.\n");
-     }
-     else if(critere==2){ 
-          int espece;*/
-          
+
         
 
       
