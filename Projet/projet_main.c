514f6cb4feac59dc0d28f7f206aa63d029803dd4
Jarjuna95 <jarjuna05@gmail.com>
2025-05-06 12:16:12 +0200

    Update projet_main.c

--- Projet/projet_main.c
+++ Projet/projet_main.c
@@ -2,9 +2,9 @@
 
 int main(){
     int i=0;
-    int nb_animaux;
+    /*int nb_animaux;
     printf("Combien y a t'il d'animal dans le reufuge actuellement");
-    scanf("%d",&nb_animaux);
+    scanf("%d",&nb_animaux);*/
     int choix;
     Animal refuge[50];
     FILE * fichier = fopen("Animal/Animaux.txt", "r");
@@ -19,6 +19,6 @@ int main(){
         printf("%d, %s, %d, %d, %f, %s \n", refuge[j].numero_id, refuge[j].nom, refuge[j].espece, refuge[j].anniv, refuge[j].poids, refuge[j].comm);
     }*/ //txt -> tableau verif
     printf("Bonjour dans l'accueil de notre refuge"\n); 
-    RechercheAnimal(refuge);
+    Menu(refuge);
      return 0;
 }
