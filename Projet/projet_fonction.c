a424fa085be4a53f62dd3ff157c4dc96afe4f2f7
Jarjuna95 <jarjuna05@gmail.com>
2025-05-09 14:28:49 +0200

    Update projet_fonction.c

--- Projet/projet_fonction.c
+++ Projet/projet_fonction.c
@@ -2,27 +2,82 @@
 
 
 void AfficheAnimal(int indice, Animal* refuge){
-        printf("%d, %s, %d, %d, %f, %s \n", refuge[indice].numero_id, refuge[indice].nom, refuge[indice].espece, refuge[indice].anniv, refuge[indice].poids, refuge[indice].comm);
+       printf("\n-------------------------------\n");
+       printf("   FICHE DE L'ANIMAL ID: %d\n", refuge[indice].numero_id);
+       printf("-------------------------------\n");
+       printf("Nom               : %s\n", refuge[indice].nom);
+       Affichageespece(refuge[indice].espece);
+       printf("Date de naissance : %d\n", refuge[indice].anniv);
+       printf("Poids             : %.2f\n", refuge[indice].poids); // pour afficher que deux decimal .2f
+       printf("A propos de lui   : %s\n", refuge[indice].comm);
 }
- 
+
+
+void Musique(){
+    const char *url = "https://www.youtube.com/watch?v=VEqkflPLVY0&autoplay=1";
+    // Commande pour ouvrir le lien dans le navigateur par défaut sous Linux
+    char commande[256];
+    snprintf(commande, sizeof(commande), "xdg-open %s", url);
+    system(commande);
+    } //fait avec Chatgpt 
+
+
+void Affichageespece(int espece){
+     if(espece==0){
+       printf("Espece            : Chien\n");
+     }
+     else if(espece==1){
+       printf("Espece            : Chat\n");
+     }
+     else if(espece==2){
+       printf("Espece            : Hamster\n");
+     }
+     else if(espece==3){
+       printf("Espece            : Dragon\n");
+     }
+}
+
 
 int Age(int h, Animal *refuge){
         if(2025-refuge[h].anniv<2){
-                  return 2;
+            return 2;
           }
         else if(2025-refuge[h].anniv>10){
-                  return 1;
+            return 1;
           }
         else{
-                  return 3;
+            return 3;
           }
    }
 
+
+void ChargerNvTab(Animal* refuge, int* taille){
+     FILE* fichier=fopen("Animal/Animaux.txt", "r");
+     if(fichier==NULL){
+        printf("Erreur lors de l'ouverture du fichier.\n");
+        exit(75);
+     }
+     fscanf(fichier,"%d", taille);
+     char ligne[300];
+     fgets(ligne, sizeof(ligne), fichier);
+     for (int i = 0; i < *taille; i++) {
+        fscanf(fichier, "%d %s %d %d %f %s",
+               &refuge[i].numero_id, refuge[i].nom, &refuge[i].espece,
+               &refuge[i].anniv, &refuge[i].poids, refuge[i].comm);
+    }
+    for (int i = *taille; i < 50; i++){
+        refuge[i].numero_id = 0;
+    } //remplir le reste avec des 0
+
+    fclose(fichier);
+ }
+     
 
 void RechercheAnimal(Animal *refuge){
      char nomRecherche[50];
      int espece, age;
      int ignore = -1;
+     int nomincorrect=0;
      printf("Entrez le nom de l'animal à chercher (écrire vide pour ignorer):\n");
      scanf("%s", nomRecherche);
         if(nomRecherche[0]>='a' && nomRecherche[0]<='z'){
@@ -45,42 +100,49 @@ void RechercheAnimal(Animal *refuge){
         }      
         for (int j = 0; j <50; j++) {                 
              if (strcmp(refuge[j].nom, nomRecherche) == 0 && refuge[j].espece==espece && Age(j,refuge)==age){                     
-                  printf("Animal trouvé voici les informations : \n");
+                  printf("\nAnimal trouvé voici les informations : \n");
                   AfficheAnimal(j,refuge);
              }
              else if(strcmp(refuge[j].nom, nomRecherche) == 0 && refuge[j].espece==espece && age==-1){
-                   printf("Voici notre sélection :\n");
+                   printf("\nVoici notre sélection :\n");
                    AfficheAnimal(j,refuge);
              }
              else if(strcmp(refuge[j].nom, nomRecherche) == 0 && espece==-1 && Age(j,refuge)==age){
-                   printf("Voici notre sélection :\n");
+                   printf("\nVoici notre sélection :\n");
                    AfficheAnimal(j,refuge);
              }
              else if(strcmp(refuge[j].nom, nomRecherche) == 0 && espece==-1 && age==-1){
-                   printf("Voici notre sélection :\n");
+                   printf("\nVoici notre sélection :\n");
                    AfficheAnimal(j,refuge);
              }
              else if(ignore==0 && refuge[j].espece==espece && Age(j,refuge)==age){
-                   printf("Voici notre sélection :\n");
+                   printf("\nVoici notre sélection :\n");
                    AfficheAnimal(j,refuge);
              }
              else if(ignore==0 && refuge[j].espece==espece && age==-1){
-                   printf("Voici notre sélection :\n");
+                   printf("\nVoici notre sélection :\n");
                    AfficheAnimal(j,refuge);
              }
              else if(ignore==0 && espece==-1 && Age(j,refuge)==age){
-                   printf("Voici notre sélection :\n");
+                   printf("\nVoici notre sélection :\n");
                    AfficheAnimal(j,refuge);
              }
              else if(ignore==0 && espece==-1 && age==-1){
-                   printf("Voici notre sélection :\n");
+                   printf("\nVoici notre sélection :\n");
                    AfficheAnimal(j,refuge);
-             }  // a verifier        
-         }
+             }// condition pour chaque recherche   
+              else if(strcmp(refuge[j].nom, nomRecherche) != 0 && strcmp(nomRecherche, "Vide") != 0){
+                   nomincorrect=1;     
+             }
      }
+     if(nomincorrect==1){
+        printf("L'animal n'existe pas !\n");
+     }
+}
 
 void Adoption(Animal *refuge){
       int id;
+      int count=0;
       printf("Saisir le numéro d'identification de l'animal à adopter:\n");
       scanf("%d", &id);
       while(id<0){
@@ -91,6 +153,7 @@ void Adoption(Animal *refuge){
       for(int p=0; p<50; p++){                
           if(refuge[p].numero_id==id){
             trouve=1;
+            count=p;
           }
       }
       if(trouve!=1){
@@ -102,64 +165,73 @@ void Adoption(Animal *refuge){
     	printf("Erreur \n");
         exit(50);
       }
+      
+      int size;
+           fscanf(ancienf,"%d", &size)==1;
+           size-=1;
+           fprintf(nouveauf,"%d\n", size);   // retire 1 dans le fichier txt    
+
+
       char ligne[300];
+      fgets(ligne, sizeof(ligne), ancienf); //saute la premiere ligne deja lu
       while(fgets(ligne, sizeof(ligne), ancienf)){
            int id_fichier;
            sscanf(ligne, "%d", &id_fichier);
-        if (id_fichier != id) {
-            fputs(ligne, nouveauf);
-        }
+           if (id_fichier != id) {
+               fputs(ligne, nouveauf);
+           }
       }
+
       fclose(ancienf);
       fclose(nouveauf);
       remove("Animal/Animaux.txt");
       rename("Animal/temporaire.txt", "Animal/Animaux.txt");
-      printf("L'animal %d a été adopté avec succès.\n", id);
-}   //demander à la prof cmt mettre a jour le tableau
+      printf("%s a été adopté est son ID est %d a été adopté avec succès.\n", refuge[count].nom,id);// probleme affichage
+} 
+
 
 void Menu(Animal* refuge){
         int continuer;
         int choix;
-        printf("Tapez 1 si vous voulez recherchez un animal, 2 pour ajoutez un animal et 3 pour adoptez un animal :"\n);
+        int size=0;
+        ChargerNvTab(refuge, &size);
+        printf("\n---------Menu---------\n");
+        printf("1-Cherchez un animal\n");
+        printf("2-Ajoutez un animal\n");
+        printf("3-Adoptez un animal\n");
+        printf("4-Nourriture nécessaire à tous les animaux\n");
+        printf("------------------------\n");
+        printf("Saisissez votre choix :\n");
         scanf("%d", &choix);
-        while(choix<1 && choix>3){
+        while(choix<1 || choix>4){
            printf("Erreur dans la saisie, veuillez ressaisir un chiffre :\n");
            scanf("%d", &choix);
         } 
+        
         if(choix==1){
            RechercheAnimal(refuge);
         }
         else if(choix==2){
            Ajouter(refuge);
-	   FILE * maj1 = fopen("Animal/Animaux.txt", "r");
-           if(maj1==NULL){
-    	     printf("Erreur \n");
-             exit(19);
-           }
-          while (fscanf(maj1, "%d %s %d %d %f %s", &refuge[i].numero_id, refuge[i].nom, &refuge[i].espece, &refuge[i].anniv, &refuge[i].poids, refuge[i].comm) == 6) {
-           i++;
-           }
-	   fclose(maj1);
         }
         else if(choix==3){
            Adoption(refuge);
-	   FILE * maj = fopen("Animal/Animaux.txt", "r");
-           if(maj==NULL){
-    	     printf("Erreur \n");
-             exit(19);
-           }
-          while (fscanf(maj, "%d %s %d %d %f %s", &refuge[i].numero_id, refuge[i].nom, &refuge[i].espece, &refuge[i].anniv, &refuge[i].poids, refuge[i].comm) == 6) {
-           i++;
-           }
-	   fclose(maj);
         }
-        printf("Vous voulez vous continuer vos recherches ? (1=oui et 0 =non) \n");
+        else if(choix==4){
+           Day_Food(refuge);
+        }
+
+        ChargerNvTab(refuge, &size);
+
+
+        printf("\nVous voulez vous continuer vos recherches ? (1=oui et 0 =non) \n");
         scanf("%d", &continuer);
         while(continuer!=1 && continuer!=0){
              printf("Erreur dans la saisie, veuillez ressaisir un chiffre :\n");
              scanf("%d", &continuer);
         } 
         if(continuer==0){
+          printf("Merci d'être passer par notre refuge !\n"); 
           return ;
         }
         else{
@@ -167,173 +239,128 @@ void Menu(Animal* refuge){
         }
 }
 
-void Day_Food(Animal refuge[],int nb_animaux){
+
+void Day_Food(Animal refuge[]){
     float croq;
-    for(int i=0; i<nb_animaux;i++){
+    for(int i=0; i<50;i++){
         if(refuge[i].espece==2){
+            printf("\n-------------------------------\n");
+            printf("  FICHE DE L'ANIMAL ID: %d\n", refuge[i].numero_id);
+            printf("-------------------------------\n");
             printf("%s a besoin de 20g de croquettes par jour.\n", refuge[i].nom);
         }
         else if(refuge[i].espece==3){
+            printf("\n-------------------------------\n");
+            printf("   FICHE DE L'ANIMAL ID: %d\n", refuge[i].numero_id);
+            printf("-------------------------------\n"); 
             printf("%s a besoin de 2.5kg de croquettes par jour.\n", refuge[i].nom);
         }
         else if(refuge[i].espece==0||refuge[i].espece==1){
             if(Age(i,refuge)==2){
                 if(refuge[i].espece==0){
-                printf("%s a besoin de 500g de croquettes par jour.\n", refuge[i].nom);
+                   printf("\n-------------------------------\n");
+                   printf("   FICHE DE L'ANIMAL ID: %d\n", refuge[i].numero_id);
+                   printf("-------------------------------\n"); 
+                   printf("%s a besoin de 500g de croquettes par jour.\n", refuge[i].nom);
             }
             else{
+                printf("\n-------------------------------\n");
+                printf("   FICHE DE L'ANIMAL ID: %d\n", refuge[i].numero_id);
+                printf("-------------------------------\n"); 
             	printf("%s a besoin de 500g de croquettes par jour.\n", refuge[i].nom);
             }
             }
             else{
                 croq=refuge[i].poids*0.1;
                 if(refuge[i].espece==0){
-                printf("%s a besoin de %f g de croquettes par jour.\n", refuge[i].nom,croq);
+                   printf("\n-------------------------------\n");
+                   printf("   FICHE DE L'ANIMAL ID: %d\n", refuge[i].numero_id);
+                   printf("-------------------------------\n") ;
+                   printf("%s a besoin de %f g de croquettes par jour.\n", refuge[i].nom,croq);
             }
-                else{
-                printf("%s a besoin de %f g de croquettes par jour.\n", refuge[i].nom,croq);
+            else{
+                 printf("\n-------------------------------\n");
+                 printf("   FICHE DE L'ANIMAL ID: %d\n", refuge[i].numero_id);
+                 printf("-------------------------------\n");
+                 printf("%s a besoin de %f g de croquettes par jour.\n", refuge[i].nom,croq);
                 }
             }
         }
   }
 }
-/*Animal Ajouter(Animal refuge[]) {
-Animal a;
-int id_unique = rand() % 101;
-int existe = 1;
-int trouve = 0; //verifie qu il y a de la place dans le refuge
-
-printf("Quelle est l'espèce de votre animal qui a été confié? (0=Chien, 1=Chat, 2=Hamster,3= Autruche)\n");
-scanf("%d",& a.espece);
- while(a.espece<-1 && a.espece>3){
-             printf("Erreur veuillez ressaisir :\n");
-             scanf("%d", &a.espece);
-        }  	
-  		
-printf("Quelle est le nom de l'animal ?\n");
-scanf("%s",a.nom);
-if(a.nom[0]>='a' && a.nom[0]<='z'){
-          a.nom[0]-=32;      
-        }
-
-printf("Quel est le poids de l'animal ?\n");
-scanf("%f", &a.poids);
- while(a.poids<0){ 
-             printf("Erreur veuillez ressaisir :\n");
-             scanf("%d", &a.poids);        
-        }      
-
-printf("Quelle est l'année de naissance ?\n");
-scanf("%d", &a.anniv);
-while(a.anniv<0){ 
-             printf("Erreur veuillez ressaisir :\n");
-             scanf("%d", &a.anniv);        
-        } 
-
-printf("Quelle est la caractéristique ?\n");
-scanf("%s", a.comm);
-
-  for (int i = 0; i < 50; i++) {
-        if (refuge[i].numero_id == 0) { // une place libre
-            while (existe=1) {
-    existe = 0; // on suppose qu'il est unique
-    for (int j = 0; j < 50; j++) {
-        if (refuge[j].numero_id == id_unique) {
-            existe = 1; // il existe déjà, donc pas bon
-            id_unique = rand() % 51; // on en génère un autre
-            break;
-        }
-    }
-}
-	    a.numero_id = id_unique;
-            refuge[i] = a;
-            trouve = 1;
-
-            FILE* fichier = fopen("Animal.txt", "a"); // on ajoute à la fin du fichier
-            if (fichier == NULL) {
-                printf("Erreur lors de l'ouverture du fichier.\n");
-                exit(1);
-            }
-
-            fprintf(fichier, "%d %s %d %d %.f %s\n",
-                    a.numero_id, a.nom, a.espece, a.anniv, a.poids, a.comm);
-            fclose(fichier);
-            printf("Animal ajouté avec succès !\n");
-            return a;
-        }
-    }
-
-    if (trouve!=1) {
-        printf("Erreur : le refuge est plein.\n");
-    }
-
-    return a;
-} // dmd a la prof cmt mettre a jour le tableau
-
-// deuxieme version
 
 
 Animal Ajouter(Animal refuge[]) {
-Animal a;
-int id_unique = rand() % 101;
-int existe = 1;
-int trouve = 0; //verifie qu il y a de la place dans le refuge
+    Animal a;
+    int trouve = 0; //verifie qu il y a de la place dans le refuge
 
-printf("Quelle est l'espèce de votre animal qui a été confié? (0=Chien, 1=Chat, 2=Hamster,3= Autruche)\n");
-scanf("%d",& a.espece);
- while(a.espece<0 && a.espece>3){
+    printf("Quelle est l'espèce de votre animal qui a été confié? (0=Chien, 1=Chat, 2=Hamster,3= Autruche)\n");
+    scanf("%d",& a.espece);
+    while(a.espece<-1 || a.espece>3){
              printf("Erreur veuillez ressaisir :\n");
              scanf("%d", &a.espece);
-        }  	
-  		
-printf("Quelle est le nom de l'animal ?\n");
-scanf("%s",a.nom);
-if(a.nom[0]>='a' && a.nom[0]<='z'){
+    }          
+                  
+    printf("Quelle est le nom de l'animal ?\n");
+    scanf("%s",a.nom);
+    if(a.nom[0]>='a' && a.nom[0]<='z'){
           a.nom[0]-=32;      
-        }
+    }
 
-printf("Quel est le poids de l'animal ?\n");
-scanf("%f", &a.poids);
- while(a.poids<0){ 
+    printf("Quel est le poids de l'animal ?\n");
+    scanf("%f", &a.poids);
+    while(a.poids<0){
              printf("Erreur veuillez ressaisir :\n");
              scanf("%f", &a.poids);        
-        }      
+    }      
 
-printf("Quelle est l'année de naissance ?\n");
-scanf("%d", &a.anniv);
-while(a.anniv<0){ 
-             printf("Erreur veuillez ressaisir :\n");
-             scanf("%d", &a.anniv);        
-        } 
-
-printf("Quelle est la caractéristique ?\n");
-scanf("%s", a.comm);
-
-  for (int i = 0; i < 50; i++) {
-        if (refuge[i].numero_id == 0) { // une place libre
-            while (existe==1) {
-    existe = 0; // on suppose qu'il est unique
-    for (int j = 0; j < 50; j++) {
-        if (refuge[j].numero_id == id_unique) {
-            existe = 1; // il existe déjà, donc pas bon
-            id_unique = rand() % 51; // on en génère un autre
-            break;
-        }
+    printf("Quelle est l'année de naissance ?\n");
+    scanf("%d", &a.anniv);
+    while(a.anniv<0){
+          printf("Erreur veuillez ressaisir :\n");
+          scanf("%d", &a.anniv);        
     }
-}
-	    a.numero_id = id_unique;
+
+    printf("Quelle est la caractéristique ?\n");
+    scanf("%s", a.comm);
+
+    for (int i = 0; i < 50; i++) {
+         if (refuge[i].numero_id == 0) { // une place libre
+            a.numero_id = 1+rand()%50;
+              for(int j=0; j<50;j++){
+                 if(refuge[j].numero_id==a.numero_id){
+                    a.numero_id = 1+rand()%50;
+                 }
+              }
             refuge[i] = a;
             trouve = 1;
 
-            FILE* fichier = fopen("Animal.txt", "a"); // on ajoute à la fin du fichier
-            if (fichier == NULL) {
-                printf("Erreur lors de l'ouverture du fichier.\n");
-                exit(1);
+            FILE * ancienf = fopen("Animal/Animaux.txt", "r");
+            FILE * nouveauf = fopen("Animal/temporaire.txt", "w"); 
+            if(ancienf==NULL || nouveauf==NULL){
+    	       printf("Erreur \n");
+               exit(51);
             }
+      
+            int size;
+            fscanf(ancienf,"%d", &size)==1;
+            size+=1;
+            fprintf(nouveauf,"%d\n", size); // rajoute +1 dans la premier ligne du txt
 
-            fprintf(fichier, "%d %s %d %d %.f %s\n",
+            char ligne[300];
+            fgets(ligne, sizeof(ligne), ancienf); //saute la premiere ligne deja lu
+            while(fgets(ligne, sizeof(ligne), ancienf)){
+                 fputs(ligne,nouveauf); // recopie ancien fichier dans le nouveau
+            }
+ 
+            fprintf(nouveauf, "%d %s %d %d %f %s\n",
                     a.numero_id, a.nom, a.espece, a.anniv, a.poids, a.comm);
-            fclose(fichier);
+            fclose(ancienf);
+            fclose(nouveauf);
+            remove("Animal/Animaux.txt");
+            rename("Animal/temporaire.txt", "Animal/Animaux.txt");
+
+
             printf("Animal ajouté avec succès !\n");
             return a;
         }
@@ -342,8 +369,5 @@ scanf("%s", a.comm);
     if (trouve!=1) {
         printf("Erreur : le refuge est plein.\n");
     }
-
     return a;
-}
-
-*/
+  }
